f195ed8fe91c9d2360dede4d194e2487f19c17fe
Dhanasri Bapatla <dhanasri.bapatla@valuelabs.net>
2012-12-19 11:22:37 +0530

    Initial commit for static page project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticPageApp::Application.config.secret_token = 'd04413b4ae12ffc18cbc92869e0a2ae77082e2e6196c929a529aca27c48f78e82bec7636ca0093fb1547616c7c6e12ed8befe30e6440db937e32c63c83154fd5'
